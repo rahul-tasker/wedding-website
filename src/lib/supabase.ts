0049db1595a19ed61a08b9777aa8bb451ed5ab12
rahul-tasker <rahultasker@gmail.com>
2026-04-05 16:06:54 -0600

    Fix syntax error in supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -5,4 +5,4 @@ const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? 'eyJhbGciOi
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
     db: { schema: 'wedding' }
-  })t 
+  })
